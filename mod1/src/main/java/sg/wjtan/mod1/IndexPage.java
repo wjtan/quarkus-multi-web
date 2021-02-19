17c782cd52ad1914c49e304f12b6c9ba99a49549
Tan Wen Jun <wjtan@users.noreply.github.com>
2021-02-20 02:21:13 +0800

    First import

---
+++ mod1/src/main/java/sg/wjtan/mod1/IndexPage.java
@@ -0,0 +1,20 @@
+package sg.wjtan.mod1;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.core.MediaType;
+
+import io.quarkus.vertx.web.Route;
+import io.quarkus.vertx.web.RouteBase;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.RoutingContext;
+
+@ApplicationScoped
+@RouteBase(path = "mod1", produces = MediaType.TEXT_HTML) 
+public class IndexPage {
+	@GET
+    @Route(path = "", methods = HttpMethod.GET)
+    void index(RoutingContext rc) {
+		rc.response().end("hello");
+    }
+}
